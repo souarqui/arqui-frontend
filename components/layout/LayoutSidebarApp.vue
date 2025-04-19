841b70942bb71f716ffe832509fe47013b928458
Cleiton Teixeira <cleiton.teixeiras@gmail.com>
2025-04-18 21:20:41 -0300

    Update LayoutSidebarApp.vue

--- components/layout/LayoutSidebarApp.vue
+++ components/layout/LayoutSidebarApp.vue
@@ -47,18 +47,11 @@ onUnmounted(() => {
               >Arqui</span
             >
             <button class="ml-auto">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 text-gray-500"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <AqIcon
+                icon="alt-arrow-up-down"
+                size="xsmall"
+                color="main-default"
+              />
             </button>
           </div>
         </transition>
